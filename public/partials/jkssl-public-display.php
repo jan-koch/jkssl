31ff5788d77120a9d23f31af997d21047700160c
Jan Koch <kochjan01@gmail.com>
2020-05-19 15:00:05 +0200

    Updated layout to load Vimeo in lightbox

--- public/partials/jkssl-public-display.php
+++ public/partials/jkssl-public-display.php
@@ -11,12 +11,18 @@
  * @package    Jkssl
  * @subpackage Jkssl/public/partials
  */
+
 ob_start();
+$title = get_the_title( $session_id );
+$video = get_post_meta( $session_id, 'ess_vimeo_url', true );
+$thumb = get_the_post_thumbnail_url( $session_id, 'full' );
 ?>
-<h1>TEST</h1>
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<div id="session-<?php echo $session_id; ?>">
-<h2><?php echo get_the_title( $session_id ); ?></h2>
+<div id="session-<?php echo $session_id; ?>" class='summit-session'>
+<a href="<?php echo $video; ?>" class="html5lightbox" data-thumbnail="<?php echo $thumb; ?> "><img src="<?php echo $thumb; ?>" alt="<?php echo $title; ?>">
+</a>
+
+<h2><?php echo $title; ?></h2>
 </div>
 
 <?php
